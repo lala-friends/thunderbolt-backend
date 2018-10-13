4e6bb4872537a399dae9877629283228c2abc6fa
ryan <whuk84@gmail.com>
2018-10-13 18:33:50 +0900

    first source commit

---
+++ src/test/java/com/lala/bolt/thunderboltbackend/ThunderboltBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lala.bolt.thunderboltbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThunderboltBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
