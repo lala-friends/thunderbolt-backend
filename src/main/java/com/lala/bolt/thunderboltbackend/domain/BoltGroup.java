25c8246a580c5f3d05c76cb9f20593670b2fba1b
ryan <whuk84@gmail.com>
2018-10-20 15:35:05 +0900

    [update] Group -> BoltGroup because reserved word and add builder

---
+++ src/main/java/com/lala/bolt/thunderboltbackend/domain/BoltGroup.java
@@ -0,0 +1,25 @@
+package com.lala.bolt.thunderboltbackend.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity(name = "BoltGroup")
+@Getter
+@Setter
+@Builder
+public class BoltGroup {
+
+    @Column(name = "id", nullable = false)
+    @Id
+    @GeneratedValue(generator = "group_seq_gen", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "group_seq_gen", sequenceName = "group_id_seq")
+    private long id;
+
+    @Column(name = "name", length = 300, nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 3000, nullable = false)
+    private String description;
+}
---
+++ src/test/java/com/lala/bolt/thunderboltbackend/domain/GroupTest.java
@@ -0,0 +1,34 @@
+package com.lala.bolt.thunderboltbackend.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GroupTest {
+    // 모든 도메인은
+    // @Getter
+    // @Setter
+    // @Builder
+
+    @Test
+    public void domain_group_member() {
+        // 그룹의 멤버는
+        // id
+        // name
+        // description
+        BoltGroup group = BoltGroup.builder()
+        .id(1)
+        .name("group1")
+        .description("group1 description")
+        .build();
+
+        assertThat(group.getId()).isEqualTo(1L);
+        assertThat(group.getName()).isEqualTo("group1");
+        assertThat(group.getDescription()).isEqualTo("group1 description");
+    }
+}
