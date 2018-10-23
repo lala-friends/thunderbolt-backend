b459997e6e9e9df7e0b945595d8ad4b135e85047
ryan <whuk84@gmail.com>
2018-10-23 14:44:37 +0900

    [update] remove jpa

--- src/main/java/com/lala/bolt/thunderboltbackend/domain/BoltGroup.java
+++ src/main/java/com/lala/bolt/thunderboltbackend/domain/BoltGroup.java
@@ -2,24 +2,18 @@ package com.lala.bolt.thunderboltbackend.domain;
 
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
 
-import javax.persistence.*;
 
-@Entity(name = "BoltGroup")
 @Getter
 @Setter
 @Builder
 public class BoltGroup {
 
-    @Column(name = "id", nullable = false)
     @Id
-    @GeneratedValue(generator = "bolt_group_seq_gen", strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "bolt_group_seq_gen", sequenceName = "bolt_group_id_seq")
     private long id;
 
-    @Column(name = "name", length = 300, nullable = false)
     private String name;
 
-    @Column(name = "description", length = 3000, nullable = false)
     private String description;
 }
--- src/main/java/com/lala/bolt/thunderboltbackend/domain/BoltUser.java
+++ src/main/java/com/lala/bolt/thunderboltbackend/domain/BoltUser.java
@@ -3,29 +3,22 @@ package com.lala.bolt.thunderboltbackend.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
-import javax.persistence.*;
 import java.util.List;
 
-//@Entity(name = "BoltUser")
+
 @Getter
 @Setter
 @Builder
 public class BoltUser {
 
-
-    @Column(name = "id", nullable = false)
     @Id
-    @GeneratedValue(generator = "bolt_user_seq_gen", strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "bolt_user_seq_gen", sequenceName = "bolt_user_id_seq")
     private long id;
 
-    @Column(name = "email", length = 300, nullable = false)
     private String email;
 
-    @Column(name = "name", length = 300, nullable = false)
     private String name;
 
-    @Column(name = "groupId")
     private List<Long> groupId;
 }
