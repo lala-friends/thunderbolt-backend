6fc8c28f06d9f2e34eb29c667fe962519a035d08
ryan <whuk84@gmail.com>
2018-10-28 13:16:58 +0900

    [update] add before

--- src/test/java/com/lala/bolt/thunderboltbackend/repository/BoltGroupRepositoryTest.java
+++ src/test/java/com/lala/bolt/thunderboltbackend/repository/BoltGroupRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.lala.bolt.thunderboltbackend.repository;
 
 import com.lala.bolt.thunderboltbackend.domain.BoltGroup;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,15 @@ public class BoltGroupRepositoryTest {
     @Autowired
     private BoltGroupRepository boltGroupRepository;
 
+    @Before
+    public void setup() {
+        boltGroupRepository.deleteAll().subscribe();
+        boltGroupRepository.save(new BoltGroup(null, "group1", "des1")).subscribe();
+    }
+
     @Test
     public void boltGroupRepositorySaveAndReadTest() {
-
-        boltGroupRepository.save(new BoltGroup(null, "group1", "des1")).block();
         Flux<BoltGroup> boltGroupFlux = boltGroupRepository.findByName("group1");
-
         StepVerifier.create(boltGroupFlux)
                 .assertNext(boltGroup -> {
                     System.out.println(boltGroup.toString());
