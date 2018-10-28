9b4f7841c115b9b21effe795c83669d0e4752413
ryan <whuk84@gmail.com>
2018-10-28 13:28:43 +0900

    [create] BoltEvent

---
+++ src/main/java/com/lala/bolt/thunderboltbackend/domain/BoltEvent.java
@@ -0,0 +1,31 @@
+package com.lala.bolt.thunderboltbackend.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BoltEvent {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String startDate;
+    private String endDate;
+    private String creator;
+    private String groupId;
+    private List<String> joinedMember;
+    private String createDate;
+    private String createUser;
+    private String updateDate;
+    private String updateUser;
+}
---
+++ src/test/java/com/lala/bolt/thunderboltbackend/domain/BoltEventTest.java
@@ -0,0 +1,39 @@
+package com.lala.bolt.thunderboltbackend.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BoltEventTest {
+
+    @Test
+    public void domain_bolt_event_member() {
+
+        List<String> joinedMember = new ArrayList<>();
+        joinedMember.add("whuk84@gmail.com");
+
+        BoltEvent boltEvent = BoltEvent.builder()
+                .title("모임1")
+                .description("주말에봐용")
+                .startDate("2018-12-31 17:00")
+                .endDate("")
+                .creator("whuk84@gmail.com")
+                .groupId("")
+                .joinedMember(joinedMember)
+                .createDate("2018-11-30 17:00")
+                .createUser("whuk84@gmail.com")
+                .updateDate("2018-11-30 17:00")
+                .updateUser("whuk84@gmail.com")
+                .build();
+
+        assertThat(boltEvent.getTitle()).isEqualTo("모임1");
+    }
+}
