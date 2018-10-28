5040ed7797e2b9ed193ebc160ab89d6c4beb8874
ryan <whuk84@gmail.com>
2018-10-28 11:41:04 +0900

    [test] data insert test

--- src/main/java/com/lala/bolt/thunderboltbackend/ThunderboltBackendApplication.java
+++ src/main/java/com/lala/bolt/thunderboltbackend/ThunderboltBackendApplication.java
@@ -1,12 +1,37 @@
 package com.lala.bolt.thunderboltbackend;
 
+import com.lala.bolt.thunderboltbackend.domain.BoltGroup;
+import com.lala.bolt.thunderboltbackend.repository.BoltGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThunderboltBackendApplication {
+public class ThunderboltBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private BoltGroupRepository boltGroupRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThunderboltBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String args[]) {
+		System.out.println("##################");
+
+		BoltGroup boltGroup = BoltGroup.builder()
+
+				.name("preban")
+				.description("sss")
+				.build();
+		boltGroupRepository.save(boltGroup).subscribe();
+
+		boltGroupRepository.findAll().log().map(BoltGroup::getDescription)
+				.subscribe(System.out::println);
+
+		boltGroupRepository.findByName("preban").log().map(BoltGroup::getDescription).subscribe(System.out::println);
+
 	}
 }
