b1247b2b094e741fa518aa30b372f1122b58ae9b
ryan <whuk84@gmail.com>
2018-11-03 14:21:14 +0900

    [update] saveBoltEvent

--- src/main/java/com/lala/bolt/thunderboltbackend/handler/BoltEventHandler.java
+++ src/main/java/com/lala/bolt/thunderboltbackend/handler/BoltEventHandler.java
@@ -5,10 +5,13 @@ import com.lala.bolt.thunderboltbackend.repository.BoltEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import java.util.UUID;
 
 @Component
 public class BoltEventHandler {
@@ -19,5 +22,30 @@ public class BoltEventHandler {
     public Mono<ServerResponse> boltEvents(ServerRequest serverRequest) {
         Flux<BoltEvent> boltEventFlux = boltEventRepository.findAll();
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(boltEventFlux, BoltEvent.class);
+    }
+
+    public Mono<ServerResponse> saveBoltEvent(ServerRequest serverRequest) {
+        Mono<BoltEvent> boltEventMono = serverRequest.bodyToMono(BoltEvent.class);
+        final UUID id = UUID.randomUUID();
+        return ServerResponse.created(UriComponentsBuilder.fromPath("/boltEvent").build().toUri())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(BodyInserters.fromPublisher(
+                        boltEventMono.map(
+                                b -> new BoltEvent(
+                                         id.toString()
+                                        ,b.getTitle()
+                                        ,b.getDescription()
+                                        ,b.getStartDate()
+                                        ,b.getEndDate()
+                                        ,b.getCreator()
+                                        ,b.getGroupId()
+                                        ,b.getJoinedMember()
+                                        ,b.getConfirm()
+                                        ,""
+                                        ,""
+                                        ,""
+                                        ,""
+                                ))
+                                .flatMap(boltEventRepository::save), BoltEvent.class));
     }
 }
